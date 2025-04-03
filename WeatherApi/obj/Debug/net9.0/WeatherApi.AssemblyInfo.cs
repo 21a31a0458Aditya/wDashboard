99d26736badbfc001663ecb3cde9f863ca480650
Aditya <21a31a0458Aditya>
2025-04-03 09:47:37 +0530

    Fixing missing folder issue

--- WeatherApi/obj/Debug/net9.0/WeatherApi.AssemblyInfo.cs
+++ WeatherApi/obj/Debug/net9.0/WeatherApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e9849e8208a4ba119573ff06943b45cde35371")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
